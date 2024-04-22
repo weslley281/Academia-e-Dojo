47ccc367699da23731d41fc8e4600f0cbef225ce
Weslley Ferraz <weslleyhenrique800@gmail.com>
2024-04-22 12:58:11 -0400

    a

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academias</title>
+    <link rel="stylesheet" href="./libs/bootstrap/bootstrap.css">
+</head>
+<body>
+    
+<script src="./libs/bootstrap/jquery.js"></script>
+    <script src="./libs/bootstrap/popper.js"></script>
+    <script src="./libs/bootstrap/bootstrap.js"></script>
+</body>
+</html>
