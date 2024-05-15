3689410f19f132dd918c177e9b573c1158cb4003
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-05-15 09:22:27 -0400

    a

--- views/class/create.php
+++ views/class/create.php
@@ -1,17 +1,17 @@
 <div class="container mt-5">
     <h1>Criar Turma</h1> <!-- Título da página -->
-    <form action="./controllers/ClassController.php?action=create" method="post" class="form-group"> <!-- Ação do formulário -->
-        <div class="mb-3 form-group"> <!-- Campo para o nome da classe -->
+    <form action="./controllers/ClassController.php?action=create" method="post"> <!-- Ação do formulário -->
+        <div class="form-group form-group"> <!-- Campo para o nome da classe -->
             <label for="name" class="form-label"><strong>Nome:</strong></label>
             <input type="text" id="name" name="name" class="form-control" required>
         </div>
 
-        <div class="mb-3"> <!-- Campo para a descrição da classe -->
+        <div class="form-group"> <!-- Campo para a descrição da classe -->
             <label for="description" class="form-label"><strong>Descrição:</strong></label>
             <textarea name="description" id="description" class="form-control" cols="30" rows="5" required></textarea>
         </div>
 
-        <div class="mb-3"> <!-- Campo para o ID da arte marcial associada -->
+        <div class="form-group"> <!-- Campo para o ID da arte marcial associada -->
             <label for="idMartialArt" class="form-label"><strong>Arte Marcial:</strong></label>
             <select name="idMartialArt" class="form-control" id="idMartialArt">
                 <?php
@@ -29,32 +29,34 @@
             </select>
         </div>
 
-        <div class="mb-3"> <!-- Campo para o ID do instrutor associado -->
+        <div class="form-group"> <!-- Campo para o ID do instrutor associado -->
             <label for="idInstructor" class="form-label"><strong>Instrutor:</strong></label>
-            <?php
-            $users = $user->getAllInstructors();
+            <select class="form-control" name="idInstructor" id="idInstructor">
+                <?php
+                $users = $user->getAllInstructors();
 
-            if (isset($users) && !empty($users)) {
-                foreach ($users as $user) {
-            ?>
-                    <option value="<?= htmlspecialchars($user['id']) ?>"><?= htmlspecialchars($user['name']) ?></option>
-            <?php
+                if (isset($users) && !empty($users)) {
+                    foreach ($users as $user) {
+                ?>
+                        <option value="<?= htmlspecialchars($user['id']) ?>"><?= htmlspecialchars($user['name']) ?></option>
+                <?php
+                    }
                 }
-            }
-            ?>
+                ?>
+            </select>
         </div>
 
-        <div class="mb-3"> <!-- Campo para a hora inicial da classe -->
+        <div class="form-group"> <!-- Campo para a hora inicial da classe -->
             <label for="initialHour" class="form-label"><strong>Hora Inicial:</strong></label>
             <input type="time" id="initialHour" name="initialHour" class="form-control" required>
         </div>
 
-        <div class="mb-3"> <!-- Campo para a hora final da classe -->
+        <div class="form-group"> <!-- Campo para a hora final da classe -->
             <label for="finalHour" class="form-label"><strong>Hora Final:</strong></label>
             <input type="time" id="finalHour" name="finalHour" class="form-control" required>
         </div>
 
-        <div class="row"> <!-- Botões para submeter ou cancelar -->
+        <div class="form-group row"> <!-- Botões para submeter ou cancelar -->
             <div class="col">
                 <button type="submit" class="mb-3 btn btn-success float-left">Criar</button> <!-- Botão de criação -->
             </div>
